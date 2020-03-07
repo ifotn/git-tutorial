9af16463119b2017709b07b7db1d31d36cdfbcba
ifotn <rich@infrontofthenet.com>
2020-03-07 15:21:13 -0500

    created index.html

--- email-test.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-mail('rich.freeman@georgiancollege.ca', "test", ' did it work?', 'From: rich.freeman@georgiancollege.ca');
-?>
--- mailtest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-mail('rich.freeman@georgiancollege.ca', 'Test Subject', 'Test Message', "From: lamp@computerstudi.es");
-
-
-?>
